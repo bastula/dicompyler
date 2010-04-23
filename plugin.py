eb78f8a3098242484f9fbf81e58e62511739e7f5
Aditya Panchal <apanchal@bastula.org>
2010-04-23 17:46:59 -0500

    Implemented first version of plugin system. All base plugins are located under the baseplugins folder. Please see dvh.py for an example of what is required of a plugin.

---
+++ plugin.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+# plugin.py
+"""Plugin manager for dicompyler."""
+# Copyright (c) 2010 Aditya Panchal
+# This file is part of dicompyler, relased under a BSD license.
+#    See the file license.txt included with this distribution, also
+#    available at http://code.google.com/p/dicompyler/
+
+import imp, os
+import wx
+from wx.xrc import *
+from model import *
+import guiutil, util
+
+def import_plugins():
+    """Find and import available plugins."""
+
+    bppath = util.GetBasePluginsPath('')
+    modules = []
+    plugins = []
+    for file in os.listdir(bppath):
+        module = file.split('.')[0]
+        if module not in modules:
+            if not (module == "__init__"):
+                modules.append(module)
+                f, filename, description = imp.find_module(module, [bppath])                
+                try:
+                    plugins.append(imp.load_module(module, f, filename, description))
+                    print 'Plugin:', module, 'loaded'
+                finally:
+                    f.close()
+    return plugins
+
+def PluginManager(parent, plugins):
+    """Prepare to show the plugin manager dialog."""
+
+    # Load the XRC file for our gui resources
+    res = XmlResource(util.GetResourcePath('plugin.xrc'))
+
+    dlgPluginManager = res.LoadDialog(parent, "PluginManagerDialog")
+    dlgPluginManager.Init(plugins)
+
+    # Show the dialog
+    dlgPluginManager.ShowModal()
+
+class PluginManagerDialog(wx.Dialog):
+    """Manage the available plugins."""
+
+    def __init__(self):
+        pre = wx.PreDialog()
+        # the Create step is done by XRC.
+        self.PostCreate(pre)
+
+    def Init(self, plugins):
+        """Method called after the panel has been initialized."""
+
+        # Set window icon
+        if not guiutil.IsMac():
+            self.SetIcon(guiutil.get_icon())
+
+        # Initialize controls
+        self.lcPlugins = XRCCTRL(self, 'lcPlugins')
+        self.btnGetMorePlugins = XRCCTRL(self, 'btnGetMorePlugins')
+        self.btnDeletePlugin = XRCCTRL(self, 'btnDeletePlugin')
+
+        self.plugins = plugins
+
+        # Bind interface events to the proper methods
+#        wx.EVT_BUTTON(self, XRCID('btnDeletePlugin'), self.DeletePlugin)
+        self.InitPluginList()
+        self.LoadPlugins()
+
+    def InitPluginList(self):
+        """Initialize the plugin list control."""
+        
+        info = wx.ListItem()
+        info.m_mask = wx.LIST_MASK_TEXT | wx.LIST_MASK_IMAGE | wx.LIST_MASK_FORMAT
+        info.m_image = -1
+        info.m_format = 0
+        
+        info.m_text = 'Name'
+        self.lcPlugins.InsertColumnInfo(0, info)
+        self.lcPlugins.SetColumn(0, info)
+        self.lcPlugins.SetColumnWidth(0, 120)
+        
+        info.m_text = 'Description'
+        self.lcPlugins.InsertColumnInfo(1, info)
+        self.lcPlugins.SetColumn(1, info)
+        self.lcPlugins.SetColumnWidth(1, 300)
+        
+        info.m_text = 'Author'
+        self.lcPlugins.InsertColumnInfo(2, info)
+        self.lcPlugins.SetColumn(2, info)
+        self.lcPlugins.SetColumnWidth(2, 150)
+        
+        info.m_text = 'Version'
+        self.lcPlugins.InsertColumnInfo(3, info)
+        self.lcPlugins.SetColumn(3, info)
+        self.lcPlugins.SetColumnWidth(3, 75)
+
+        info.m_text = 'Enabled'
+        self.lcPlugins.InsertColumnInfo(4, info)
+        self.lcPlugins.SetColumn(4, info)
+        self.lcPlugins.SetColumnWidth(4, 75)
+
+    def LoadPlugins(self):
+        """Update and load the data for the plugin list control."""
+
+        # Set up the plugins for each plugin entry point of dicompyler
+        for n, p in enumerate(self.plugins):
+            props = p.pluginProperties()
+            self.lcPlugins.InsertStringItem(n, props['name'])
+            self.lcPlugins.SetStringItem(n, 1, props['description'])
+            self.lcPlugins.SetStringItem(n, 2, props['author'])
+            self.lcPlugins.SetStringItem(n, 3, str(props['version']))
+            self.lcPlugins.SetStringItem(n, 4, 'Yes')
