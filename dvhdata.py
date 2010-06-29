9d871258aaf55f0ead4dad0b858e532773ac3339
Aditya Panchal <apanchal@bastula.org>
2010-06-28 23:54:10 -0500

    Added independent volume calculation for structures. This fixes issue 11.

--- dvhdata.py
+++ dvhdata.py
@@ -49,35 +49,58 @@ def CalculateVolume(structure):
     # Store the total volume of the structure
     sVolume = 0
 
-    # Store the total number of planes to be used later
-    sLen = len(sPlanes)
     n = 0
     # Iterate over each plane in the structure
     for sPlane in sPlanes.itervalues():
 
-        # Store the total area for each plane
-        sArea = 0
+        # Calculate the area for each contour in the current plane
+        contours = []
+        largest = 0
+        largestIndex = 0
+        for c, contour in enumerate(sPlane):
+            # Create arrays for the x,y coordinate pair for the triangulation
+            x = []
+            y = []
+            for point in contour['contourData']:
+                x.append(point[0])
+                y.append(point[1])
 
-        # Create arrays for the x,y coordinate pair for the triangulation
-        x = []
-        y = []
+            cArea = 0
+            # Calculate the area based on the Surveyor's formula
+            for i in range(0, len(x)-1):
+                cArea = cArea + x[i]*y[i+1] - x[i+1]*y[i]
+            cArea = abs(cArea / 2)
+            contours.append({'area':cArea, 'data':contour['contourData']})
 
-        for point in sPlane['contourData']:
-            x.append(point['x'])
-            y.append(point['y'])
+            # Determine which contour is the largest
+            if (cArea > largest):
+                largest = cArea
+                largestIndex = c
 
-        # Calculate the area based on the Surveyor's formula
-        for i in range(0, len(sPlane['contourData'])-1):
-            sArea = sArea + x[i]*y[i+1] - x[i+1]*y[i]
-        sArea = abs(sArea / 2)
+        # See if the rest of the contours are within the largest contour
+        area = contours[largestIndex]['area']
+        for i, contour in enumerate(contours):
+            # Skip if this is the largest contour
+            if not (i == largestIndex):
+                contour['inside'] = False
+                for point in contour['data']:
+                    if PointInPolygon(point[0], point[1], contours[largestIndex]['data']):
+                        contour['inside'] = True
+                        # Assume if one point is inside, all will be inside
+                        break
+                # If the contour is inside, subtract it from the total area
+                if contour['inside']:
+                    area = area - contour['area']
+                # Otherwise it is outside, so add it to the total area
+                else:
+                    area = area + contour['area']
 
         # If the plane is the first or last slice
         # only add half of the volume, otherwise add the full slice thickness
-        if ((n == 0) or (n == sLen)):
-            sVolume = float(sVolume) + float(sArea) * float(structure['thickness']) * 0.5
+        if ((n == 0) or (n == len(sPlanes))):
+            sVolume = float(sVolume) + float(area) * float(structure['thickness']) * 0.5
         else:
-            sVolume = float(sVolume) + float(sArea) * float(structure['thickness'])
-
+            sVolume = float(sVolume) + float(area) * float(structure['thickness'])
         # Increment the current plane number
         n = n + 1
 
@@ -85,3 +108,24 @@ def CalculateVolume(structure):
     volume = sVolume/1000
 
     return volume
+
+def PointInPolygon(x, y, poly):
+    """Uses the Ray Casting method to determine whether a point is within
+        the given polygon.
+        Taken from: http://www.ariel.com.au/a/python-point-int-poly.html"""
+
+    n = len(poly)
+    inside = False
+    p1x, p1y, p1z = poly[0]
+    for i in range(n+1):
+        p2x, p2y, p2z = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
