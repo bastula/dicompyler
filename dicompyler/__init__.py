485a430dbbfcfbf098de4a863c133c759ed14705
Aditya Panchal <apanchal@bastula.org>
2011-12-29 01:15:06 -0600

    Move files to new directory layout in prepration for Python packaging.

---
+++ dicompyler/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# dicompyler.py
+"""Main file for dicompyler."""
+# Copyright (c) 2009-2011 Aditya Panchal
+# This file is part of dicompyler, relased under a BSD license.
+#    See the file license.txt included with this distribution, also
+#    available at http://code.google.com/p/dicompyler/
+
+from main import start
+
+if __name__ == '__main__':
+    import dicompyler.main
+    dicompyler.main.start()
